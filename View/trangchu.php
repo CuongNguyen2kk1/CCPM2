8d38a52f8a0d0d14cb11cc2e57456635dd667d74
CuongNguyen2kk1 <89174864+CuongNguyen2kk1@users.noreply.github.com>
2022-12-19 10:26:23 +0700

    Nhanh1

--- View/trangchu.php
+++ View/trangchu.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">    
     <link rel="stylesheet" href="style.css">
     <title>Nhóm 1</title>
-    <title>NHANH2</title>
+    <title>NHANH1</title>
 </head>
 
 <body>
